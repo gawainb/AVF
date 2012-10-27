d319d90279932f47bc921ad73fc38309c8cf79de
Gawain Bracy II <g2musicgroup@fullsail.edu>
2012-10-27 00:14:00 -0400

    Updated folder
    Signed-off-by: Gawain Bracy II <g2musicgroup@fullsail.edu>

---
+++ College Selector/College Selector/College_Selector.h
@@ -0,0 +1,13 @@
+//
+//  College_Selector.h
+//  College Selector
+//
+//  Created by Gawain Bracy II on 10/25/12.
+//  Copyright (c) 2012 Gawain Bracy II. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface College_Selector : NSObject
+
+@end
