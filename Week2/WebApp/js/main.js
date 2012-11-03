467eeef04ed1789bd281fa10e8b45e9bd3134ab3
Gawain Bracy II <g2musicgroup@fullsail.edu>
2012-11-02 22:39:44 -0400

    built other pages for app.
    Signed-off-by: Gawain Bracy II <g2musicgroup@fullsail.edu>

---
+++ Week2/WebApp/js/main.js
@@ -0,0 +1,23 @@
+/* Gawain Bracy II
+*/
+
+$(document).ready(function()
+{
+	//HOME PAGE
+	$('#home').live('pageshow', function()
+	{console.log("Home Page Loaded");}); //end contact pageinit
+
+	//ADD HEADER
+	$('#header').empty();
+	$(function()
+	{$('#header').append('<center><h1><a href="index.html">AVF Launch Page</a></h1></center>');});
+	console.log("Header Loaded");
+	
+	//ADD FOOTER
+	$('#footer').empty();
+	$(function()
+	{$('#footer').append('<small>Gawain Bracy II : AVF1210</small>');});
+	console.log("Footer Loaded");
+
+	
+});
